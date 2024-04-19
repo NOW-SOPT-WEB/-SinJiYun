2d82ed472eb8e38ffef0eb490a178b9f59b94619
Sinji <sinzi031012@naver.com>
2024-04-19 18:09:02 +0900

    Feat: 장바구니 구매 모달 생성

--- week1/shopping-list/cart.js
+++ week1/shopping-list/cart.js
@@ -9,6 +9,12 @@ const cartItems = JSON.parse(localStorage.getItem('cartitems'));
 const tableClass = document.querySelector(".table");
 
 // 구매하기 버튼
+const buyBtn = document.querySelector(".buyBtn");
+
+// 모달
+const buyModal = document.querySelector(".buyModal");
+const buyModalClose = document.querySelector(".modalClose");
+const buyItem = document.querySelector(".modalItem");
 
 // 사이드바 
 sideBtn.addEventListener("click", () => {
@@ -28,14 +34,13 @@ let cartItemCard = cartItems.map(item => {
     return `
     <tr>
     <td><input type="checkbox" class="checkbox"></td>
-    <td><img src="${item.image}" style="width: 3rem;"></td>
+    <td><img src="${item.image}" style="width: 4rem;"></td>
     <td>${item.title}</td>
     <td>${item.price.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</td>
     <td>${item.category}</td>
     <td><button class="delete">삭제</button></td>
     </tr>
     `
-    console.log(cartItems)
 });
 
 tableClass.innerHTML += cartItemCard.join('');
@@ -52,3 +57,29 @@ deleteBtns.forEach((deleteBtn, index) => {
         location.href='./cart.html';
     });
 });
+
+// 구매
+buyBtn.addEventListener("click", () => {
+    buyModal.classList.remove("buyModalClose");   // 먼저 삭제해야 함!
+    buyModal.classList.add("buyModalOpen");
+    console.log("모달 열음");
+});
+
+// 모달 닫기
+buyModalClose.addEventListener("click", () => {
+    buyModal.classList.remove("buyModalOpen");   // 먼저 삭제해야 함!
+    buyModal.classList.add("buyModalClose");
+    console.log("모달 닫음");
+});
+
+let buyItems = cartItems.map(item => {
+    return `
+    <article class="buyItem">
+        <img src="${item.image}" alt="${item.title}">
+        <h4>${item.title}</h4>
+        <p>${item.price}원</p>
+    </article>
+    `;
+});
+
+buyItem.innerHTML += buyItems.join('');
