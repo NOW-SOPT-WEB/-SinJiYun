7843726c534ae59cb1f109f42f7c303e9ace30fd
Sinji <sinzi031012@naver.com>
2024-04-22 20:44:02 +0900

    Chore: 필요없는 파일 삭제 및 기본 세팅

---
+++ week3/src/layout/Header.jsx
@@ -0,0 +1,11 @@
+// import styled from 'styled-components';
+
+const Header = () => {
+    return(
+        <header>
+            <h1>같은 카드 맞추기</h1>
+        </header>
+    )
+}
+
+export default Header
