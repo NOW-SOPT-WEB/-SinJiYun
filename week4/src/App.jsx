630f74151f12bca8767f52e3fd123d38d271d2a6
Sinji <sinzi031012@naver.com>
2024-05-10 03:35:26 +0900

    Feat: 라우터 연결

--- week4/src/App.jsx
+++ week4/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <GlobalStyle />
+      <RouterProvider router={router} />
     </>
   )
 }
